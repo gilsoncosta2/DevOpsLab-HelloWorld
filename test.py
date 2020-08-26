69bb0317801b46226fd940fe0678d7a56cd87472
gilson.gcosta2 <gilson.gcosta2@gmail.com>
2020-08-26 14:58:09 -0400

    UPDATE Readmi

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Foi alterada por Gilson Alvino da Costa em 26.08.2020 às 14h59min.")
 
 
 if __name__ == "__main__":
